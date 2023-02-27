ec8f095f633838ae90d977723648a72674bbbed2
CLeeBenjamin <castonboyd@gmail.com>
2023-02-27 13:53:30 -0500

    dbConfig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import APIURL from './apiEnd';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
 
   
   useEffect(() => {
-    fetch("http://localhost:3001/todos")
+    fetch(APIURL)
     .then(result => result.json())
     .then(data => setTasks(data))
   });
